81c7ada0159e69155a845d408d4064a7604f8882
jiayuan929 <252461528@qq.com>
2023-01-31 15:58:04 +0800

    fix: 回退staticfiles_urlpatterns

--- backend/urls.py
+++ backend/urls.py
@@ -36,6 +36,7 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns  # noqa
 from django.shortcuts import redirect
 from django.urls import path
 from django.views.i18n import JavaScriptCatalog
