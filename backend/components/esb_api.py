f504781258b1cba318d6c414c18e51b677673b20
jiayuan929 <252461528@qq.com>
2023-06-06 19:55:49 +0800

    feat: esbchannel 的配置从 API 中获取

---
+++ backend/components/esb_api.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+TencentBlueKing is pleased to support the open source community by making
+蓝鲸智云 - 蓝鲸桌面 (BlueKing - bkconsole) available.
+Copyright (C) 2022 THL A29 Limited,
+a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+
+We undertake not to change the open source license (MIT license) applicable
+
+to the current version of the project delivered to anyone in the future.
+"""
+from components.esb import _call_esb_api
+from components.http import http_get
+
+
+def get_weixin_config():
+    """
+    获取微信配置
+    """
+    path = '/api/c/compapi/esb/get_weixin_config/'
+    _ok, _, _message, data = _call_esb_api(http_get, path, {})
+    return data
