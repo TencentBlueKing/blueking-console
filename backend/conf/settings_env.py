ce5839779fb5e854f1207fa3cd82ddc84cc7f6a4
jiayuan929 <252461528@qq.com>
2025-06-23 17:02:18 +0800

    feat: add tls

--- backend/conf/settings_env.py
+++ backend/conf/settings_env.py
@@ -41,6 +41,26 @@ DATABASES = {
     }
 }
 
+# 数据库 ssl 配置
+BK_PAAS_DATABASE_TLS_ENABLED = env.bool("BK_PAAS_DATABASE_TLS_ENABLED", False)
+if BK_PAAS_DATABASE_TLS_ENABLED:
+    default_ssl_options = {
+        "ca": env.str("BK_PAAS_DATABASE_TLS_CERT_CA_FILE", ""),
+    }
+    # mTLS
+    default_cert_file = env.str("BK_PAAS_DATABASE_TLS_CERT_FILE", "")
+    default_key_file = env.str("BK_PAAS_DATABASE_TLS_CERT_KEY_FILE", "")
+    if default_cert_file and default_key_file:
+        default_ssl_options["cert"] = default_cert_file
+        default_ssl_options["key"] = default_key_file
+    # 跳过主机名/IP 验证，会降低安全性，正式环境不能开启
+    check_hostname = env.bool("BK_PAAS_DATABASE_TLS_CHECK_HOSTNAME", True)
+    default_ssl_options["check_hostname"] = check_hostname
+
+    if "OPTIONS" not in DATABASES["default"]:
+        DATABASES["default"]["OPTIONS"] = {}
+    DATABASES["default"]["OPTIONS"]["ssl"] = default_ssl_options
+
 # 是否展示蓝鲸产品版本信息
 IS_BK_SUITE_ENABLED = env.bool("IS_BK_SUITE_ENABLED", False)
 if IS_BK_SUITE_ENABLED:
